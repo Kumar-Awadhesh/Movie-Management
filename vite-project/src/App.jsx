dc5ab8e67457690568dcfd7ef940937db4acb0bb
Awadhesh Kumar <kumaravi0506@gmail.com>
2025-06-10 03:47:01 +0530

    search facility added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 function App() {
   const [movies, setMovies] = useState([]);
   const [page, setPage] = useState(1);
+  const [input, setInput] = useState("")
   let perPage = 12;
 
   const fetchMovies = async(page) => {
@@ -28,15 +29,54 @@ function App() {
     fetchMovies(nxtPage);
   }
 
+  const searchMovie = async() => {
+    let currentPage = page;
+    let filteredMovies = [...movies]; 
+    while(true){
+      let filteredArr = filteredMovies.filter((ele) => ele.movie.toLowerCase().includes(input.toLowerCase()));
+      if(filteredArr.length > 0){
+        setMovies(filteredArr);
+        break;
+      }
+
+      currentPage += 1;
+      try {
+        
+        const newFetch = await axios.get(`https://movie-hub-backend-w0l9.onrender.com/movies?_page=${currentPage}&_perPage=${perPage}`)
+        const newMovies = newFetch.data.Movies;
+        filteredMovies = [...filteredMovies, ...newMovies]
+
+        if(newMovies.length < 1){
+        console.log("no result found!")
+        break;
+      }
+      } 
+      catch (err) {
+        console.error("catch error!", err)
+        break;
+      }
+      
+    }
+  
+    console.log(filteredMovies)
+  }
+
   return (
     <>
       <main className='container'>
+        <div className='title'>
+          <h1>Movie Theatre</h1>
+        </div>
+        <div className='search-bar'>
+          <input type="text" placeholder='Search movie...' required value={input} onChange={(e) => {setInput(e.target.value)}}/>
+          <button onClick={searchMovie}>Search</button>
+        </div>
         <div className='movie-container'>
           {movies.map((ele, index) =>(
             <div key={index}>
               <img src={`https://movie-hub-backend-w0l9.onrender.com${ele.image}`} alt="" />
-              <h2>{ele.movie}</h2>
-              <h3>{ele.released}</h3>
+              <h2>Moive Name: {ele.movie}</h2>
+              <h3>Released Year: {ele.released}</h3>
             </div>
           ))}
         </div>
